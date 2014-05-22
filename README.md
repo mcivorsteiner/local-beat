15aeabcbe36257fe9be31b078620564f1aaab2ba
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-22 16:10:30 -0700

    initial rails 3.2.13 skeleton

---
+++ README.md
@@ -0,0 +1,14 @@
+#LocalBeat
+Connect to the live music around you.
+
+####Created By:
+Chris Yancey  
+Fatima Khalilova   
+Kaelin Burns  
+Ki Steiner   
+Mia Garbagnati  
+ 
+ 
+ 
+  
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalBeat::Application.config.secret_token = 'c6af9d69fd53c2e878405d4f7fbd3b46bcbabd431aae2ca2f72239564b5cf7f22abb94d9f3bbdf3a01ff499a9645d57e9219249bf6b7cabe1a35221facf194d8'
