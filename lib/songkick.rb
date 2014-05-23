40db609969dc2f0d264bf46e673f30b007439a2c
& <pair+cyancey.nickmeehan@devbootcamp.com>
2014-05-23 14:09:07 -0700

    create event_search query to find events by artist and date

--- lib/songkick.rb
+++ lib/songkick.rb
@@ -11,14 +11,14 @@ module Songkick
        "/search/locations.json?query=#{query}&apikey=#{ENV["SONGKICK_KEY"]}"
     end
 
-    def get(url, query = {})
-      response = HTTParty.get(url, query: query)
+    def request(url, query = {})
+      response = get(url, query: query)
       JSON.parse(response.body)
     end
 
     def location_id_query(location_query)
       url = location_search_url(location_query)
-      response = get(url)
+      response = request(url)
       locations = response["resultsPage"]["results"]["location"].map do |location|
         location_name = location["metroArea"]["displayName"] rescue nil
         sk_location_id = location["metroArea"]["id"].to_i rescue nil
@@ -31,11 +31,31 @@ module Songkick
       locations
     end
 
-    # def event_search(query = {})
-    #   url = "/events.json?apikey=#{ENV["SONGKICK_KEY"]}"
-    #   response = get(url, query)
-    #   pp response
-    #   events = response["resultsPage"]["results"]["event"]
-    #   extract_event_data(events)
-    # end
+    def event_search(query = {})
+      url = "/events.json?apikey=#{ENV["SONGKICK_KEY"]}"
+      response = request(url, query)
+      events = response["resultsPage"]["results"]["event"]
+      extract_event_data(events)
+    end
+
+
+    def extract_event_data(events)
+        events_data = events.map do |event|
+          sk_event_id = event["id"].to_i rescue nil
+          display_name = event["displayName"] rescue nil
+          date = event['start']['date'] rescue nil
+          artists = event["performance"].map { |performance| performance["artist"]["displayName"]} rescue nil
+          metro_area = event["venue"]["metroArea"]["displayName"] rescue nil
+          state = event["venue"]["metroArea"]["state"]["displayName"] rescue nil
+          # metro_area_loc = [event["venue"]["metroArea"]["lat"].to_f, event["venue"]["metroArea"]["lng"].to_f] rescue nil
+          venue_name = event["venue"]["displayName"] rescue nil
+          popularity = event["popularity"].to_f rescue nil
+          location = [event["location"]["lat"].to_f, event["location"]["lng"].to_f] rescue nil
+          event_type = event["type"] rescue nil
+          uri = event["uri"] rescue nil
+          { sk_event_id: sk_event_id, display_name: display_name, date: date, artists: artists, metro_area: metro_area, state: state, venue_name: venue_name, popularity: popularity, location: location, event_type: event_type, uri: uri }
+        end
+        events_data.reject { |event| event[:location][0] == 0 }
+      end
+
 end
