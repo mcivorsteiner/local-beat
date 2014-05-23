1ab5f848d66cda424db6124996eb52680402f755
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-05-23 01:09:16 -0700

    center mark on US

--- app/assets/javascripts/mapView.js
+++ app/assets/javascripts/mapView.js
@@ -6,7 +6,7 @@ mapView.prototype = {
   drawMap: function(){
     var mapOptions = {
     zoom: 5,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    center: new google.maps.LatLng(41.850033, -87.6500523)
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     }
