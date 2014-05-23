b4072700e277723fcaafa3913896265401f0a4ee
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-05-22 23:15:13 -0700

    added user auth to user (model and controller) and session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    if @user && @user.save
+      session[:user_id] = @user.id
+      ##Determine logged in action
+    else
+      # Determine login failure action
+      # render :new
+    end
+  end
 end
