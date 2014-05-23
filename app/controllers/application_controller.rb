3644585f1dc66319e4e39b0df237d81c729de5bd
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-05-22 23:27:27 -0700

    added current_user and logged_in? helper methods for use in views and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
