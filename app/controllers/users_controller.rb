92c33e5ff67ec30e651b69bafa0f678d4d945e72
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-05-23 14:50:59 -0700

    created partial for user_data which includes a script tag containing logged_in user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.password = params[:password]
-    if @location = Location.find_by_user_input_location_name(params[:location])
+    user_input_location_name = "#{params[:city]}, #{params[:state]}"
+    puts user_input_location_name
+    if @location = Location.find_by_user_input_location_name(user_input_location_name)
       @user.location = @location
     else
-      # find location via SongKick API
-      # display matches to user?
+      # location_query_results = SongKick.location_id_query(user_input_location_name)
+      # top_location_match = location_query_results.first
+      # @location = Location.new(user_input_location_name: user_input_location_name## Parse other location data from query)
+
       # set @user.location equal to SongKick Location ID
     end
 
 
     if @user && @user.save
       session[:user_id] = @user.id
-      ##Determine logged in action
+      render partial: "shared/user_data", locals: {user: current_user, user_location_data: @user_location_data}
     else
       # Determine login failure action
       # render :new
