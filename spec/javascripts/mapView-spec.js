ac36c212e7f3d66e2fc91a9e7ffd642c789c7c7b
& <pair+Szarko.jhulley@devbootcamp.com>
2014-05-23 11:42:10 -0700

    Fixed SpecRunner.html to run Jasmine tests. Started framework of mapView-spec

---
+++ spec/javascripts/mapView-spec.js
@@ -0,0 +1,8 @@
+describe("Google Maps View", function() {
+  it("displays a map from the google maps api", function() {
+    // expect something
+  })
+  xit("centers on the middle of the US", function() {
+    // expect something
+  })
+})
