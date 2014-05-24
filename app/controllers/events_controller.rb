e465f1725f765566499d55a96cb91a1e2a98c2ec
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-24 16:49:41 -0700

    RED added helper methods to EventsController format date fields recieved from client side

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
-  def search #params => {location: ___, artist_name: ____, min_date: ____, max_date:___}
+  def search
     unless @location = Location.find_by_user_input_location_name(params[:user_input_location_name])
       @location = Songkick.location_id_query(params[:user_input_location_name]).first
       @location.save
     end
-    @events = Songkick.event_search({ location: "sk:#{@location.sk_location_id}", artist_name: params[:artist_name], min_date: params[:min_date], max_date: params[:max_date]})
+    event_query = format_event_query(params, @location)
+    @events = Songkick.event_search(event_query)
     render json: { location_coords: { lat: @location.lat, lng: @location.lng }, events: @events }
+  end
+
+  private
+
+  def format_event_query(params, location)
+    location = "sk:#{location.sk_location_id}"
+    min_date = format_date(params[:min_date])
+    max_date = format_date(params[:max_date])
+    return {location: location, artist_name: params[:artist_name], min_date: min_date, max_date: max_date}
+  end
+
+  def format_date(date)
+    return "" if date == ""
+    year = date["(1i)"]
+    month = date["(2i)"]
+    day = date["(3i)"]
+    formatted_date = '%04d-%02d-%02d' % [year, month, day]
   end
 end
