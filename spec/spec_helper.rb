eacec9bfd02738cbd986fd523dc7f0ea39377b7a
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-23 23:10:50 -0700

    refactor and added test helper file for events controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ require 'songkick'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  # config.include TestHelpers
+  config.include TestHelpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
