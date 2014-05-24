1c2e280340225f47f21a7615e0ac855d58f35368
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-05-24 16:49:21 -0700

    added UserController tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe UsersController do
+  User.destroy_all
+  let!(:user) { FactoryGirl.create :user }
+
+  context "sign-up" do
+    it "responds with success when passed valid attributes" do
+      Songkick.stub(:location_id_query) { [FactoryGirl.create(:location)] }
+      params = {user: {email: "notreal@fake.com" },
+                password: "password"}
+      post :create, params
+      expect(response).to be_success
+    end
+
+    it "has unsuccessful response when passed invalid email" do
+      Songkick.stub(:location_id_query) { [FactoryGirl.create(:location)] }
+      params = {user: {email: "invalid@nope.x" },
+                password: "password"}
+
+      post :create, params
+      expect(response).to_not be_success
+    end
+
+    it "has unsuccessful response when password not present" do
+      Songkick.stub(:location_id_query) { [FactoryGirl.create(:location)] }
+      params = {user: {email: "notreal@fake.com" },
+                password: ""}
+
+      post :create, params
+      p response
+      expect(response).to_not be_success
+    end
+
+  end
+end
