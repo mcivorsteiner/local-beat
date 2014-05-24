1450333b628065a7cd96dde8e86c7b9f189182e2
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-24 01:07:01 -0700

    added session_controller_spec with three tests, also added user to FactoryGirl

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe SessionController do
+  let!(:user) { FactoryGirl.create :user }
+  
+  context "create(login)" do
+    it "assigns @user to correct user" do
+      post :create, { :email => user.email, :password => user.password }
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "creates a new session with correct credentials" do
+      post :create, { :email => user.email, :password => user.password }
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it "doesn't create a new session with invalid credentials" do
+      session[:user_id] = nil
+      post :create, { email: user.email, password: "#{user.password}x"}
+      expect(session[:user_id]).to eq(nil)
+    end
+  end
+end
