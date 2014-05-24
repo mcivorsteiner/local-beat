7400a7ddb6047b11968353383af496b82d472b41
Kaelin Burns <kaelinbu@gmail.com>
2014-05-24 12:10:31 -0700

    fix merge conflict

---
+++ app/assets/javascripts/applicationController.js
@@ -0,0 +1,13 @@
+ApplicationController = function(mapController, sessionController, searchController) {
+  this.mapController = mapController
+  this.sessionController = sessionController
+  this.searchController = searchController
+}
+
+ApplicationController.prototype= {
+  init: function() {
+    this.mapController.init()
+    this.sessionController.init()
+    this.searchController.init()
+  }
+}
