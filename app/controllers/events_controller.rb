280c1574cba2aa35326818dfb4b3df06a442de70
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-23 23:32:43 -0700

    add more tests for search route in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def search #params => {location: ___, artist_name: ____, min_date: ____, max_date:___}
-    unless @location = Location.find_by_user_input_location_name(params[:location])
-      @location = Songkick.location_id_query(params[:location]).first
+    unless @location = Location.find_by_user_input_location_name(params[:user_input_location_name])
+      @location = Songkick.location_id_query(params[:user_input_location_name]).first
       @location.save
     end
     @events = Songkick.event_search({ location: "sk:#{@location.sk_location_id}", artist_name: params[:artist_name], min_date: params[:min_date], max_date: params[:max_date]})
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -11,6 +11,22 @@ describe EventsController do
       get :search
       expect(response).to be_success
     end
+
+    it "creates a location if it is not found in the database" do
+      Songkick.stub(:location_id_query) { [FactoryGirl.create(:location)] }
+      Songkick.stub(:event_search) { create_event_data }
+      expect{ 
+      get :search
+      }.to change { Location.count }.by(1)
+    end
+
+    it "does not create a new location if it is already in the database" do
+      
+      Songkick.stub(:event_search) {create_event_data}
+      expect{
+        get :search, user_input_location_name: location.user_input_location_name
+      }.to change { Location.count }.by(0)
+    end
   end
 
 end
