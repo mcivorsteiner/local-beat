37297a238c6d74d83068f5bd9112b842a70fb035
& <pair+Szarko.jhulley@devbootcamp.com>
2014-05-24 22:27:29 -0700

    changed map syle

--- app/assets/javascripts/mapView.js
+++ app/assets/javascripts/mapView.js
@@ -3,14 +3,75 @@ function MapView(){
 
 MapView.prototype = {
   defaults: {
-    lng: 40.689493,
-    lat: -98.578265,
+    lat: 40.689493,
+    lng: -98.578265,
   },
 
   drawMap: function(){
+    var image = '/images/marker-icon.png'
     var mapOptions = {
       zoom: 5,
-      center: new google.maps.LatLng(this.defaults.lng, this.defaults.lat)
+      center: new google.maps.LatLng(this.defaults.lat, this.defaults.lng),
+      marker: new google.maps.Marker({icon: image}),
+      styles: [
+      {
+        "featureType":"water",
+        "stylers": [
+          {"visibility":"on"},
+          {"color":"#b5cbe4"}
+          ]},
+      {
+        "featureType":"landscape",
+        "stylers": [
+          {"color":"#efefef"}
+          ]},
+      {
+        "featureType":"road.highway",
+        "elementType":"geometry",
+        "stylers": [
+          {"color":"#83a5b0"}
+          ]},
+      {
+        "featureType":"road.arterial",
+        "elementType":"geometry",
+        "stylers": [
+        {"color":"#bdcdd3"}
+        ]},
+      {
+        "featureType":"road.local",
+        "elementType":"geometry",
+        "stylers": [
+          {"color":"#ffffff"}
+          ]},
+      {
+        "featureType":"poi.park",
+        "elementType":"geometry",
+        "stylers": [
+        {"color":"#e3eed3"}
+        ]},
+      {
+        "featureType":"administrative",
+        "stylers": [
+        {"visibility":"on"},
+        {"lightness":33}
+        ]},
+      {
+        "featureType":"road"
+      },
+      {
+        "featureType":"poi.park",
+        "elementType":"labels",
+        "stylers": [
+        {"visibility":"on"},
+        {"lightness":20}
+        ]},
+      {},
+      {
+        "featureType":"road",
+        "stylers": [
+        {"lightness":20}
+        ]}
+      ]
     };
     this.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   },
@@ -19,6 +80,17 @@ MapView.prototype = {
     for (var i = 0; i < markers.length; i++) {
       markers[i].setMap(this.map);
     }
+  },
+
+  setMap: function(locationCoords){
+    var lng = parseFloat(locationCoords.lng)
+    var lat = parseFloat(locationCoords.lat)
+    var center = new google.maps.LatLng(lat, lng)
+    this.map.panTo(center)
+    this.map.setZoom(12)
+    // var image = '/images/marker-icon.png'
+    // var marker = new google.maps.Marker({icon: image})
+
   }
 }
 
