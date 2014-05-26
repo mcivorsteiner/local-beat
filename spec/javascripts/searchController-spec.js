9bf67dbbf99d34694f85085be6090b85f31b8cab
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-25 18:42:58 -0700

    modified spec files, added searcView spec

--- spec/javascripts/searchController-spec.js
+++ spec/javascripts/searchController-spec.js
@@ -1,18 +1,12 @@
 describe("Search controller", function(){
-    var searchController
+    var searchController, searchView
     beforeEach(function(){
-      searchController = new SearchController()
+      searchView = new SearchView()
+      searchController = new SearchController(searchView)
     });
 
     it("should always have a setListeners method", function() {
       expect(searchController.setListeners).toBeDefined()
     });
 
-    it("should always have a renderSearchBar method", function() {
-      expect(searchController.renderSearchBar).toBeDefined()
-    });
-
-    it("should always have a renderAdvancedOptions method", function() {
-      expect(searchController.renderAdvancedOptions).toBeDefined()
-    });
 });
