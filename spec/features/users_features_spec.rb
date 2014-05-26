75256a39618b280d83fa6abaea4c9c5aa04e920e
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-25 22:15:16 -0700

    added one users_features test, not currently passing, pending

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe "Users", :js => true do
   context "sign up" do
-    it "allow user user to sign up" do
+
+    xit "allow user user to sign up" do
       visit root_path
       find('#user-session').click
+      find('#sign-up').click
+      fill_in "Email", :with => "mia@mia.com"
+      fill_in "Password", :with => "mia"
+      fill_in "City", :with => "San Francisco"
+      fill_in "State", :with => "CA"
+      find("#new_user input[type='submit']").click
+      expect(page.body).to have_selector('#user-data')
     end
   end
 end
