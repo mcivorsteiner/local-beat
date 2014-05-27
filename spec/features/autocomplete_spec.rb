ecca4d98b761654dad4a8ede489fc95aaced329e
& <pair+Szarko.jhulley@devbootcamp.com>
2014-05-26 23:46:39 -0700

    added a non-working capybara test for autocomplete

--- spec/features/autocomplete_spec.rb
+++ spec/features/autocomplete_spec.rb
@@ -1,1 +1,17 @@
 # require 'spec_helper'
+
+# describe "Artist Search", :js => true do
+
+#   context "search text field" do
+#     before(:each) { visit root_path }
+#     it "should autocomplete as user types in artist search text field" do
+#       find('#magnify').click
+#       fill_in "Location", :with => "Los Angeles"
+#       find('#more-options-icon').click
+#       fill_in "Artist Name", :with => "billy "
+#       sleep(5)
+
+#       page.should have_selector('ul.ui-autocomplete li.ui-menu-item')
+#     end
+#   end
+# end
