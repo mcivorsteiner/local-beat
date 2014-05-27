83b597e25d6c41e1dfa09d8f6afb0eb01fbca4d4
Kaelin Burns <kaelinbu@gmail.com>
2014-05-26 18:14:21 -0700

    render date time readable

--- app/assets/javascripts/models/eventModel.js
+++ app/assets/javascripts/models/eventModel.js
@@ -2,7 +2,7 @@ function Event(eventData){
   this.constructor.count++
   this.skEventId = eventData.sk_event_id
   this.displayName = eventData.display_name
-  this.date = eventData.date
+  this.date = this.dateFormatter(eventData.date)
   this.artists = eventData.artists
   this.latLng = eventData.location
   this.metroArea = eventData.metro_area
@@ -39,6 +39,19 @@ Event.prototype = {
     } else {
         return time += ' AM';
     }
+  },
+
+  dateFormatter: function(date) {
+    var parts = date.split('-')
+      year = parts[0],
+      month = parts[1]-1,
+      day = parts[2];
+
+    var monthName = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+    return monthName[month]+ " " + day + ", " + year
+
+
   }
 
 }
