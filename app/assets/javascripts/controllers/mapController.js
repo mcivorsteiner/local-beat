c66913a1865dfc9ef226d13d1c41e32795d267cb
Kaelin Burns <kaelinbu@gmail.com>
2014-05-26 22:30:29 -0700

    add grey transparent infobox for small view

--- app/assets/javascripts/controllers/mapController.js
+++ app/assets/javascripts/controllers/mapController.js
@@ -29,20 +29,37 @@ MapController.prototype = {
   },
 
   showInfoWindow: function() {
-    if (typeof infoWindow != "undefined") {
-      infoWindow.close()
+    if (typeof infoBox != "undefined") {
+      infoBox.close()
     }
+    var infoBox;
+    var boxOptions = { disableAutoPan: false
+    ,maxWidth: 0
+    ,pixelOffset: new google.maps.Size(-140, 0)
+    ,zIndex: null
+    ,boxStyle: { opacity: 0.75
+     }
+    ,closeBoxMargin: "10px 2px 2px 2px"
+    ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
+    ,infoBoxClearance: new google.maps.Size(1, 1)
+    ,isHidden: false
+    ,pane: "floatPane"
+    ,alignBottom: true
+    ,enableEventPropagation: false
+      };
+    infoBox = new InfoBox(boxOptions);
     var eventDetails = this.eventInfo
     if (eventDetails.isFestival()) {
       var html = HandlebarsTemplates['events/small_festival_info_box'](eventDetails)
     } else {
       var html = HandlebarsTemplates['events/small_event_info_box'](eventDetails)
     }
-    infoWindow = new google.maps.InfoWindow()
-    infoWindow.setContent(html)
-    infoWindow.open(this.map, this)
-  }
+    infoBox.setContent(html);
+    infoBox.open(this.map, this);
+
+
+
+
+ }
 }
-
-
 
