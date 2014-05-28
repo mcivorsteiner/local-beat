a019705f5e900e1413a03e28a9fe8be07876850a
Fatima Khalilova <fatimakhalilova@gmail.com>
2014-05-28 16:47:06 -0700

    changed the place of the require simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'simplecov'
+SimpleCov.start
+puts "required simplecov"
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'songkick'
